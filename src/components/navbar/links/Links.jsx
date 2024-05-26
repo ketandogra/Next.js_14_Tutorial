a1cf72a2fd8d5b28d155ac1cee82d846d19b2a66
ketan dogra <ketandogra1367@gmail.com>
2024-05-26 21:29:52 +0530

    implemented responsive navbar design

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,19 +1,25 @@
-
-
+"use client"
+import { useState } from "react";
 import styles from "./links.module.css"
 import NavLink from "./navLink/navLink";
 
+const links = [
+  { title: "Homepage", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+  { title: "Blog", path: "/blog" },
+];
+
 const Links = () => {
-  const links = [
-    { title: "Homepage", path: "/" },
-    { title: "About", path: "/about" },
-    { title: "Contact", path: "/contact" },
-    { title: "Blog", path: "/blog" },
-  ];
+
+  const [open,setOpen] = useState(false)
+
   const session = true;
   const isAdmin = true;
 
   return (
+
+    <div className={styles.container}>
     <div className={styles.links}>
       {links.map((link) => (
         <NavLink item={link} key={link.title}/>
@@ -21,6 +27,19 @@ const Links = () => {
       {
         session ? <> {isAdmin && (<NavLink item={{title: "Admin", path:"/admin"}} />)} <button className={styles.logout}>Logout</button> </> : (<NavLink item={{title: "Login", path:"/login"}} />)
       }
+    </div>
+    <button className={styles.menuButton} onClick={()=>setOpen((prev)=>!prev)}>Menu</button>
+    {
+      open && <div className={styles.mobileLinks}>
+        {
+          links.map((link) => (
+            <NavLink item={link} key={link.title}/>
+          ))
+        }
+        </div>
+
+    }
+
     </div>
   );
 };
